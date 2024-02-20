9f16e0de6113d3987722e1232155904103a8b327
kucu-try <baekfive@gmail.com>
2024-02-20 18:12:16 +0900

    catfact

--- 02_component/todolist/src/Home/Semi.js
+++ 02_component/todolist/src/Home/Semi.js
@@ -1,11 +1,15 @@
-import { Navigation, Pagination } from 'swiper/modules';
-import Swiper from 'swiper';
-import SwiperSlide  from 'swiper/react';
 
-import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './index.css';
+
+// import required modules
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import Title from "../Introduce/Title";
 
 const style = {
@@ -31,21 +35,18 @@ const Semi = () => {
             <h1 style={style}>학교 종이 땡땡땡</h1>
             <div style={asideColor} />
             <div>
-                <Swiper
-                    // install Swiper modules
-                    modules={[Navigation, Pagination]}
-                    spaceBetween={50}
-                    slidesPerView={3}
-                    navigation
-                    pagination={{ clickable: true }}
-                    scrollbar={{ draggable: true }}
-                    onSwiper={(swiper) => console.log(swiper)}
-                    onSlideChange={() => console.log('slide change')}
+            <Swiper
+                les={[Navigation, Pagination, Autoplay]}
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
                 >
-                    <SwiperSlide>Slide 1</SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    ...
-                </Swiper>
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
             </div>
 
         </>
--- catfact/src/Catfact.js
+++ catfact/src/Catfact.js
@@ -8,8 +8,8 @@ import { useEffect, useState } from "react";
 
 const CatFact = ()=>{
     const [fact1, setFact] =useState('');
-    const initImage = "https://cataas.com/cat/pbrosoqOlUUtR5XJ";
-    const [catImg, setCatImg] =useState(initImage);
+    // const initImage = "https://cataas.com/cat/pbrosoqOlUUtR5XJ";
+    // const [catImg, setCatImg] =useState(initImage);
     
    
        const onClickHandler = ()=>{
@@ -21,13 +21,13 @@ const CatFact = ()=>{
                 // setCatImg("https://cataas.com/cat");
             });
             
-            fetch(`https://cataas.com/cat?json=true`)
-            .then(response1 => response1.json())
-            .then(data1=>{
-                console.log(data1.mimetype + "d");
-                setCatImg(data1.mimetype);
+            // fetch(`https://cataas.com/cat?json=true`)
+            // .then(response1 => response1.json())
+            // .then(data1=>{
+            //     console.log(data1.mimetype + "d");
+            //     setCatImg(data1.mimetype);
                 
-            });
+            // });
        }
           
   
@@ -36,7 +36,7 @@ const CatFact = ()=>{
         <>
             <h3>오늘의 고양이 사실</h3>
             <h4>{fact1}</h4>
-            <img style={{width: '400px', height: '400px'}} src={catImg}/>
+            {/* <img style={{width: '400px', height: '400px'}} src={catImg}/> */}
             <button onClick={onClickHandler}>다시 고르기</button>
         </>
     )
@@ -45,51 +45,3 @@ const CatFact = ()=>{
 export default CatFact;
 
 
-
-
-
-
-
-
-
-
-
-// const { useState } = require("react")
-
-
-// const SearchBox =({setFactContent})=>{
-//     const [catFact , setCatFact] = useState('');
-
-
-//     const onClickHandler = ()=>{
-//         // fetch("https://api.github.com/emojis")
-//         fetch("https://cat-fact.herokuapp.com/facts")
-//         .then(r=> r.json())
-//         .then(result => setFactContent(result[catFact.facts]));
-//     }
-
-
-//     return (
-//         <>
-//             <button onClick={onClickHandler}>사실 뽑기</button>
-//         </>
-//     )
-// }
-
-// const ContentBox=({factContent})=>{
-//     return !!factContent? <h1>{factContent}</h1> : <h1>텅</h1>
-// }
-
-// const CatFact =() =>{
-//     const [factContent, setFactContent] = useState('');
-
-//     return (
-//         <>
-//             <SearchBox setFactContent={setFactContent}/>
-//             <br/>
-//             <ContentBox factContent={factContent}/>
-//         </>
-//     )
-// }
-
-// export default CatFact;
